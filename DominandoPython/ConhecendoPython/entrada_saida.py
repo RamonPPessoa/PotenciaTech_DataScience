e27082360e63d3fe02a65f620a58046f0fefeac7
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-08-03 17:22:14 -0400

    Trabalhando com entrada e saida de dados

---
+++ DominandoPython/ConhecendoPython/entrada_saida.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+
+idade = int(input("Digite sua idade: "))
+
+print(nome,idade)
+print(nome,idade,end="... \n")
+print(nome,idade,sep="#",end="...\n")
+print(nome,idade,sep="#")
