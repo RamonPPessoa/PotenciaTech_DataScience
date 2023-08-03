d3e81a0ffdc2b657fe58374e3132d9f522e2002a
Ramon Pessôa <ramon.palheta.pessoa@hotmail.com>
2023-08-03 15:25:18 -0400

    Bootcamp Potência Tech by iFood | Ciência de Dados com Python

---
+++ DominandoPython/ConhecendoPython/01-variaveis_constantes.py
@@ -0,0 +1,19 @@
+idade = 35
+nome = 'Paula'
+
+print(f'Meu nome e  {nome} e eu tenho {idade} anos')
+
+
+print("\n**********************")
+
+idade,nome =(35,'Paula') # OU idade,nome = 35,'Paula'
+print(f'Meu nome e  {nome} e eu tenho {idade}  anos')
+
+
+
+print("************ CONSTANTES***********")
+
+
+
+ESTADOS = ["AM","AC",'SP']
+print(ESTADOS)
